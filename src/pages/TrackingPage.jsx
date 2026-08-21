3bddee9d9f94ea452aecaae9e4dd7ea907193a56
FIO de Bengal <dev@fiodebengal.com>
2026-08-21 06:00:11 +0600

    style: fix logos with direct SVGs and increase global font size

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -46,9 +46,9 @@ const TrackingPage = () => {
 
   const getCarrierLogo = (carrier) => {
     switch (carrier) {
-      case 'FedEx': return 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/FedEx_Express.svg/512px-FedEx_Express.svg.png';
-      case 'DHL': return 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/DHL_Express_logo.svg/512px-DHL_Express_logo.svg.png';
-      case 'UPS': return 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/UPS_logo.svg/512px-UPS_logo.svg.png';
+      case 'FedEx': return 'https://upload.wikimedia.org/wikipedia/commons/9/9d/FedEx_Express.svg';
+      case 'DHL': return 'https://www.dhl.com/content/dam/dhl/global/core/images/logos/dhl-logo.svg';
+      case 'UPS': return 'https://www.ups.com/assets/resources/images/UPS_logo.svg';
       default: return null;
     }
   };
